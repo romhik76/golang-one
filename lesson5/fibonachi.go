cd08b3e0490098214aba5cf06100f15dcb49c50e
romhik76 <64015552+romhik76@users.noreply.github.com>
2021-12-07 13:54:20 +0300

    myMap

--- lesson5/fibonachi.go
+++ lesson5/fibonachi.go
@@ -2,26 +2,36 @@ package main
 
 import "fmt"
 
-func fibonachi(n uint) uint {
+var myMap = make(map[uint]uint)
 
+func Fibonachi(n uint) uint {
 	if n == 0 {
 		return 0
 	}
 	if n == 1 {
 		return 1
 	}
-	return fibonachi(n-1) + fibonachi(n-2)
+	val, ok := myMap[n]
+	if ok {
+		fmt.Println(fmt.Sprintf("map: %d", myMap[n]))
+
+		return val
+	}
+	myMap[n] = Fibonachi(n-1) + Fibonachi(n-2)
+	fmt.Println(fmt.Sprintf("fin: %d", myMap[n]))
+
+	return myMap[n]
 }
 
-func number() {
+func Number() {
 	var n uint
 	fmt.Println("Введите число n")
 	fmt.Scanln(&n)
 
-	fmt.Println(fmt.Sprintf("result for %d: %d", n, fibonachi(n)))
-	number()
+	fmt.Println(fmt.Sprintf("result for %d: %d", n, Fibonachi(n)))
+	Number()
 }
 func main() {
-	number()
+	Number()
 
 }
